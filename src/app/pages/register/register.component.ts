f995f70be4d532c7b213ae9416f61715f30f8d22
vf <l_vafi@yahoo.com>
2017-06-02 11:41:06 -0700

    completed register method in register.compontnt

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,7 +5,25 @@ import { RegisterService } from '../../services/register.service';
 import {Colonist} from '../../models/colonist';
 import { ColonistService } from '../../services/colonist.service';
 
+import {FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl} from '@angular/forms';
 
+
+
+const canBe = (value: string): ValidatorFn => {
+  return(control: AbstractControl) => {
+     return control.value === value ? {'cant\'be this value': value}: null;
+  };
+};
+
+const age = (tooYoung: number, tooOld: number): ValidatorFn => {
+  if (tooYoung < 0 || tooOld < 0) {
+    throw new Error('You can\'t be negative age...');
+  }
+  return (control: AbstractControl) => {
+    return control.value < 0 || control.value < tooYoung || control.value > tooOld ?
+      { 'You\'re not the right age...': age } : null;
+  };
+};
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -14,25 +32,50 @@ import { ColonistService } from '../../services/colonist.service';
 })
 export class RegisterComponent implements OnInit {
   public listJobs: Job[] = [];
-  public colonist: Colonist; 
+  colonist: Colonist; 
+  registerForm: FormGroup;
+  jobs: Job[];
+  NO_JOB_SELECTED = 'no job selected';
  
   constructor(private registerService: RegisterService,
-              private ColonistService : ColonistService) { }
+              private colonistService : ColonistService,
+              private formBuilder: FormBuilder) {   }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(100), 
+        Validators.minLength(3)
+        ]),
+      age: new FormControl('', [
+        Validators.required, age(16, 85)]),
+      job_id: new FormControl(this.NO_JOB_SELECTED, [canBe(this.NO_JOB_SELECTED)])
+    });
     this.registerService.getData()  
         .subscribe((jobs) => {
           this.listJobs = jobs;
         });
   }
+  
+  
 
-  postColonist() {
-    const colonist = new Colonist('Mak', '33', '1');
-    this.ColonistService
+  register(e) {
+    e.preventDefault();
+    if(this.registerForm.invalid) {
+      console.log('')
+    } else {
+    const name = this.registerForm.get('name').value;
+    const age = this.registerForm.get('age').value;
+    const job_id = this.registerForm.get('job_id').value;
+
+    const colonist: Colonist = {name, age, job_id}
+    this.colonistService
         .postData(colonist)
-        .subscribe((newColonist) => {
-          console.log(newColonist);
-        });
+        .subscribe((data) => {
+          localStorage.setItem('colonistIdNum', (data.colonist.id).toString());
+          console.log(localStorage.getItem('colonistIdNum'));
+        });}
   } 
  
 
